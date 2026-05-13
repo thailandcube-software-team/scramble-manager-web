2ebbb73411cec517fbc2b82733e66dbd30ec5c7d
kinpkt <phakhinthon@gmail.com>
2026-05-13 15:47:41 +0700

    Fix bugs on activity index ordering

--- scripts/services/ScrambleSorter.js
+++ scripts/services/ScrambleSorter.js
@@ -13,66 +13,74 @@ export class ScrambleSorter {
         const innerZip = new JSZip();
         const loadedPDFs = await innerZip.loadAsync(this.#file.content);
 
-        const startDate = this.#wcif.schedule.startDate;
-        const numberOfDays = this.#wcif.schedule.numberOfDays;
-        let activityIndex = 1;
-
         console.log('Loaded PDFS:', loadedPDFs);
 
+        const allActivities = [];
+
         for (const venue of this.#wcif.schedule.venues) {
-            const venueFolder = baseFolder.folder(venue.name);
-
             for (const room of venue.rooms) {
-                const roomFolder = venueFolder.folder(room.name);
-
                 for (const activity of room.activities) {
-                    const [eventID, round] = activity.activityCode.split('-');
-                    if (eventID !== 'other') {
-                        if (activity.startTime) {
-                            const dateStr = activity.startTime.split('T')[0];
-                            const dateFolder = roomFolder.folder(dateStr);
+                    const eventID = activity.activityCode.split('-')[0];
+                    
+                    if (eventID !== 'other' && activity.startTime) {
+                        allActivities.push({
+                            venueName: venue.name,
+                            roomName: room.name,
+                            activity: activity
+                        });
+                    }
+                }
+            }
+        }
 
-                            const folderName = `${activityIndex++}-${EventCodeToFullMap[eventID.toUpperCase()]}-${round.toUpperCase()}`;
-                            const eventRoundFolder = dateFolder.folder(folderName);
+        allActivities.sort((a, b) => {
+            return new Date(a.activity.startTime) - new Date(b.activity.startTime);
+        });
 
-                            if (activity.childActivities.length !== 0) {
-                                for (const childActivity of activity.childActivities) {
-                                    const groupParts = childActivity.activityCode.split('-g');
-                                    
-                                    if (groupParts.length > 1) {
-                                        const groupNumber = parseInt(groupParts[1], 10);
+        let activityIndex = 1;
 
-                                        const setLetter = String.fromCharCode(64+groupNumber);
+        for (const item of allActivities) {
+            const activity = item.activity;
+            const [eventID, round] = activity.activityCode.split('-');
+            
+            const venueFolder = baseFolder.folder(item.venueName);
+            const roomFolder = venueFolder.folder(item.roomName);
+            
+            const dateStr = activity.startTime.split('T')[0];
+            const dateFolder = roomFolder.folder(dateStr);
 
-                                        const roundNumber = round.replace('r', '');
-                                        const eventName = EventCodeToFullMap[eventID.toUpperCase()];
+            const folderName = `${activityIndex++}-${EventCodeToFullMap[eventID.toUpperCase()]}-${round.toUpperCase()}`;
+            const eventRoundFolder = dateFolder.folder(folderName);
 
-                                        let expectedFileName = '';
+            if (activity.childActivities.length !== 0) {
+                for (const childActivity of activity.childActivities) {
+                    const groupParts = childActivity.activityCode.split('-g');
+                    
+                    if (groupParts.length > 1) {
+                        const groupNumber = parseInt(groupParts[1], 10);
+                        const setLetter = String.fromCharCode(64 + groupNumber);
+                        const roundNumber = round.replace('r', '');
+                        const eventName = EventCodeToFullMap[eventID.toUpperCase()];
 
-                                        if (eventID === '333mbf') {
-                                            const attemptNumber = parseInt(groupParts[1].split('-a')[1], 10);
+                        let expectedFileName = '';
 
-                                            expectedFileName = `${eventName} Round ${roundNumber} Scramble Set ${setLetter} Attempt ${attemptNumber}.pdf`;
-                                        }
-                                        else 
-                                            expectedFileName = `${eventName} Round ${roundNumber} Scramble Set ${setLetter}.pdf`;
+                        if (eventID === '333mbf') {
+                            const attemptNumber = parseInt(groupParts[1].split('-a')[1], 10);
+                            expectedFileName = `${eventName} Round ${roundNumber} Scramble Set ${setLetter} Attempt ${attemptNumber}.pdf`;
+                        } else {
+                            expectedFileName = `${eventName} Round ${roundNumber} Scramble Set ${setLetter}.pdf`;
+                        }
 
-                                        const matchedZipEntry = Object.values(loadedPDFs.files).find(zipFile => 
-                                            !zipFile.dir && zipFile.name.includes(expectedFileName)
-                                        );
+                        const matchedZipEntry = Object.values(loadedPDFs.files).find(zipFile => 
+                            !zipFile.dir && zipFile.name.includes(expectedFileName)
+                        );
 
-                                        if (matchedZipEntry) {
-                                            const pdfBinaryData = await matchedZipEntry.async('arraybuffer');
-                                            
-                                            eventRoundFolder.file(expectedFileName, pdfBinaryData);
-                                            
-                                            console.log(`Moved: ${expectedFileName}`);
-                                        } 
-                                        else 
-                                            console.warn(`Missing PDF for: ${expectedFileName}`);
-                                    }
-                                }
-                            }
+                        if (matchedZipEntry) {
+                            const pdfBinaryData = await matchedZipEntry.async('arraybuffer');
+                            eventRoundFolder.file(expectedFileName, pdfBinaryData);
+                            console.log(`Moved: ${expectedFileName}`);
+                        } else {
+                            console.warn(`Missing PDF for: ${expectedFileName}`);
                         }
                     }
                 }
